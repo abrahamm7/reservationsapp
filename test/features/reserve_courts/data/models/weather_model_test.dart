2bbc80f9bb49e04516ab5fdf7d32a811547549bf
Abraham Morillo <abrahammorillo777@gmail.com>
2021-11-08 21:33:12 -0400

    [FIX] Test methods

--- test/features/reserve_courts/data/models/weather_model_test.dart
+++ test/features/reserve_courts/data/models/weather_model_test.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:reservationsapp/features/reserve_courts/data/models/reservation_model.dart';
+import 'package:reservationsapp/features/reserve_courts/data/models/weather_model.dart';
 import 'package:reservationsapp/features/reserve_courts/domain/entities/reservation.dart';
 import 'package:reservationsapp/features/reserve_courts/domain/entities/weather.dart';
 
@@ -19,8 +20,35 @@ void main() {
   late Current current = Current(
       condition: condition, pressureIn: 3.4, precipMm: 2, precipIn: 2, uv: 2);
 
-  final weatherModel = Weather(location: location, current: current);
-  test('Test subclass weather model', () {
-    expect(weatherModel, isA<Weather>());
+  final weatherModel = WeatherModel(location: location, current: current);
+  final conditionModel = ConditionModel();
+  final locationModel = LocationModel(
+      name: 'santo domingo',
+      region: '',
+      country: 'Dominican Republic',
+      lat: 3.5,
+      lon: 5.2,
+      tzId: '3',
+      localtimeEpoch: 2,
+      localtime: '8:00');
+
+  final currentModel = CurrentModel(
+      condition: condition, pressureIn: 3.4, precipMm: 2, precipIn: 2, uv: 2);
+
+  group('Test subclasses', () {
+    test('Test subclass weather model', () {
+      expect(weatherModel, isA<Weather>());
+    });
+    test('Test subclass condition model', () {
+      expect(conditionModel, isA<Condition>());
+    });
+
+    test('Test subclass condition model', () {
+      expect(locationModel, isA<Location>());
+    });
+
+    test('Test subclass condition model', () {
+      expect(currentModel, isA<Current>());
+    });
   });
 }
