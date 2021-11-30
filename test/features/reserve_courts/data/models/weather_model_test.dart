10bfdbe8105d80a023806e7c57e1f0eae60a6587
Abraham Morillo <abrahammorillo777@gmail.com>
2021-11-30 09:41:30 -0400

    [REM] import

--- test/features/reserve_courts/data/models/weather_model_test.dart
+++ test/features/reserve_courts/data/models/weather_model_test.dart
@@ -1,10 +1,6 @@
-import 'dart:convert';
-
 import 'package:flutter_test/flutter_test.dart';
 import 'package:reservationsapp/features/reserve_courts/data/models/forecastWeather_model.dart';
 import 'package:reservationsapp/features/reserve_courts/domain/entities/forecastWeather.dart';
-
-import '../../../../core/fixtures/fixture_reader.dart';
 
 void main() {
   late Location location = Location(
